bb3c784dc26f159fde31193e03cfd4a5b6c8d6d8
Caue Antonio <shandownpacksz13@gmail.com>
2025-09-15 10:58:21 -0300

    Adicionado o metodo de multiplicacao

--- Caculadora_Devops/src/Calculadora/Calculadora.java
+++ Caculadora_Devops/src/Calculadora/Calculadora.java
@@ -2,13 +2,14 @@ package Calculadora;
 
 public class Calculadora {
 
-    // Método de subtração
-    public static int subtracao(int a, int b) {
-        return a - b;
+    // Método de multiplicação
+    public static int multiplicacao(int a, int b) {
+        return a * b;
     }
 
     public static void main(String[] args) {
-        int resultado = subtracao(5, 3);
-        System.out.println("Subtração: " + resultado);
+        int resultado = multiplicacao(5, 3);
+        System.out.println("Multiplicação: " + resultado);
     }
 }
+

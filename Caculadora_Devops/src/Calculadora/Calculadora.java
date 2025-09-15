e592d7aa620efa9d528b57acd7237677b60ee867
Caue Antonio <shandownpacksz13@gmail.com>
2025-09-15 10:57:13 -0300

    Adicionado o metodo de subtração

--- Caculadora_Devops/src/Calculadora/Calculadora.java
+++ Caculadora_Devops/src/Calculadora/Calculadora.java
@@ -2,14 +2,13 @@ package Calculadora;
 
 public class Calculadora {
 
-    // Método de soma
-    public static int soma(int a, int b) {
-        return a + b;
+    // Método de subtração
+    public static int subtracao(int a, int b) {
+        return a - b;
     }
 
     public static void main(String[] args) {
-        int resultado = soma(5, 3);
-        System.out.println("Soma: " + resultado);
+        int resultado = subtracao(5, 3);
+        System.out.println("Subtração: " + resultado);
     }
 }
-

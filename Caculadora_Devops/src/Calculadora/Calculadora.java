16d0def27ebd5e080de6810de86b5d87645eea22
Caue Antonio <shandownpacksz13@gmail.com>
2025-09-15 10:31:54 -0300

    Adicionado o metodo de subtração da calculadora na brench "Metodo Subtração"

--- Caculadora_Devops/src/Calculadora/Calculadora.java
+++ Caculadora_Devops/src/Calculadora/Calculadora.java
@@ -6,7 +6,7 @@ import java.math.BigDecimal;
 public class Calculadora {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("=== Calculadora (apenas soma) ===");
+        System.out.println("=== Calculadora (apenas subtração) ===");
 
         try {
             System.out.print("Digite o primeiro número: ");
@@ -15,7 +15,7 @@ public class Calculadora {
             System.out.print("Digite o segundo número: ");
             BigDecimal b = new BigDecimal(sc.nextLine().trim().replace(',', '.'));
 
-            BigDecimal resultado = a.add(b);
+            BigDecimal resultado = a.subtract(b);
             System.out.println("Resultado: " + resultado.toPlainString());
         } catch (NumberFormatException ex) {
             System.out.println("Entrada inválida. Use um número válido (ex: 12.5 ou 10).");
